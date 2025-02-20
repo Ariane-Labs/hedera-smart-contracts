4e528166f430b92b77e2c6516274c097d4e9c09d
mp-arianelabs <michal.prawda@arianelabs.com>
2025-02-20 10:16:38 +0100

    feat: modify block stat for adding single txs, add tests (#13)
    Signed-off-by: mp-arianelabs <michal.prawda@arianelabs.com>

--- scripts/ethereum-shadow-network/transaction-checker/src/utils/block-stat.ts
+++ scripts/ethereum-shadow-network/transaction-checker/src/utils/block-stat.ts
@@ -2,16 +2,23 @@ import { CsvLogger } from './file-logger';
 
 interface BlockStats {
 	blockCount: number;
-	totalTime: number;
-	totalGasUsed: number;
-	totalTxFees: number;
+	totalBlockTime: number;
+	totalEthTxFees: number;
+	totalHederaTxFees: number;
 }
 
-interface AddBlockParams {
+interface TransactionParams {
 	blockNumber: number;
-	timePerBlock: number;
-	gasUsed: number;
-	txFees: number;
+	txTime: number;
+	ethTxFee: number;
+	HederaTxFee: number;
+}
+
+interface CurrentBlock {
+	blockNumber: number;
+	blockTime: number;      
+	ethTxFees: number;      
+	hederaTxFees: number;   
 	txCount: number;
 }
 
@@ -22,47 +29,87 @@ export class BlockStat {
 		{
 			header: [
 				'blockNumber',
-				'timePerBlock',
-				'gasUsed',
-				'txFees',
+				'blockTime',
+				'ethTxFees',
+				'hederaTxFees',
 				'txCount',
-				'avgTimePerBlock',
-				'totalGasUsed',
-				'totalTxFees',
+				'avgBlockTime',
+				'totalEthTxFees',
+				'totalHederaTxFees',
 			],
 		}
 	);
 
 	private static readonly stats: BlockStats = {
 		blockCount: 0,
-		totalTime: 0,
-		totalGasUsed: 0,
-		totalTxFees: 0,
+		totalBlockTime: 0,
+		totalEthTxFees: 0,
+		totalHederaTxFees: 0,
 	};
+
+	private static currentBlock: CurrentBlock | null = null;
 
 	private constructor() {}
 
-	public static addBlockLog({
-		blockNumber,
-		timePerBlock,
-		gasUsed,
-		txFees,
-		txCount,
-	}: AddBlockParams): void {
-		BlockStat.stats.blockCount++;
-		BlockStat.stats.totalTime += timePerBlock;
-		BlockStat.stats.totalGasUsed += gasUsed;
-		BlockStat.stats.totalTxFees += txFees;
+	/**
+	 * Finalizes the current block by updating overall statistics and logging the block summary.
+	 */
+	private static finalizeCurrentBlock(): void {
+		if (!this.currentBlock) return;
+
+		this.stats.blockCount++;
+		this.stats.totalBlockTime += this.currentBlock.blockTime;
+		this.stats.totalEthTxFees += this.currentBlock.ethTxFees;
+		this.stats.totalHederaTxFees += this.currentBlock.hederaTxFees;
 
 		this.BLOCK_STATS_LOGGER.info([
-			blockNumber,
-			timePerBlock,
-			gasUsed,
-			txFees,
-			txCount,
-			BlockStat.stats.totalTime / BlockStat.stats.blockCount,
-			BlockStat.stats.totalGasUsed,
-			BlockStat.stats.totalTxFees,
+			this.currentBlock.blockNumber,
+			this.currentBlock.blockTime,
+			this.currentBlock.ethTxFees,
+			this.currentBlock.hederaTxFees,
+			this.currentBlock.txCount,
+			this.stats.totalBlockTime / this.stats.blockCount,
+			this.stats.totalEthTxFees,
+			this.stats.totalHederaTxFees,
 		]);
+	}
+
+	/**
+	 * Initializes the current block with the provided transaction parameters.
+	 * @param params The transaction parameters to initialize the block.
+	 */
+	private static initCurrentBlock(params: TransactionParams): void {
+		const { blockNumber, txTime, ethTxFee, HederaTxFee } = params;
+		this.currentBlock = {
+			blockNumber,
+			blockTime: txTime,
+			ethTxFees: ethTxFee,
+			hederaTxFees: HederaTxFee,
+			txCount: 1,
+		};
+	}
+
+	/**
+	 * Adds a transaction to the current block. If the incoming transaction is from a new block,
+	 * finalizes the previous block and starts a new one.
+	 * @param params Object containing blockNumber, txTime, ethTxFee, and HederaTxFee.
+	 */
+	public static addTransaction(params: TransactionParams): void {
+		const { blockNumber, txTime, ethTxFee, HederaTxFee } = params;
+
+		if (!this.currentBlock) {
+			this.initCurrentBlock(params);
+			return;
+		}
+
+		if (this.currentBlock.blockNumber === blockNumber) {
+			this.currentBlock.blockTime += txTime;
+			this.currentBlock.ethTxFees += ethTxFee;
+			this.currentBlock.hederaTxFees += HederaTxFee;
+			this.currentBlock.txCount++;
+		} else {
+			this.finalizeCurrentBlock();
+			this.initCurrentBlock(params);
+		}
 	}
 }
